b679d63ff22562932baee1cbc8e80ee749e4c42c
Marius <marius@example.com>
2022-02-24 10:23:49 +0200

    init uzd

---
+++ js/dom.js
@@ -0,0 +1,14 @@
+console.log(document);
+
+// nusitaikyk i subtitle
+// document.getElementById('title').innerHTML = 'Pakeiciau is JS';
+const daugybaEl = document.getElementById('daugyba');
+const titleEl = document.getElementById('title');
+
+titleEl.innerHTML = 'Pakeiciau is JS';
+titleEl.innerHTML = '<button>click me</button>';
+
+// for cikas nuo 5 - 12 ir sudauginam
+const result = 4445;
+
+daugybaEl.innerHTML = result;
---
+++ js/main.js
@@ -0,0 +1,13 @@
+console.log('main.js');
+
+// parasom for cikla kuris paima skaicius nuo 0 iki 4 imtinai
+// sudedam ir gaunam bendra suma
+let total = 0;
+// debugger;
+for (let i = 0; i <= 4; i++) {
+  // let total = 0;
+  // console.log(`cycle number ${i + 1} total: ${total} + i: ${i}`);
+  total = total + i;
+  // console.log(` === ${total}`);
+}
+console.log('total ===', total);
